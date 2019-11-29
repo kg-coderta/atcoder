a11a41a1009be799171c870ffdba38009879a4a0
kg <ke123iji@gmail.com>
2019-11-29 23:35:13 +0900

    answer 64

---
+++ abc/64.rb
@@ -0,0 +1,16 @@
+A
+r,g,b = gets.chomp.split.map(&:to_i)
+num = g*10 + b
+  if num % 4 == 0 
+    puts "YES"
+  else
+    puts "NO"
+  end  
+B
+n = gets.chomp.to_i
+a = gets.split.map(&:to_i)
+p a.max - a.min
+C
+D
+E
+F
