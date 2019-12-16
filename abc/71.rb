227d4b980629e9973f3fdd5118034d4917381b4a
kg <ke123iji@gmail.com>
2019-12-16 22:21:42 +0900

    answer71

--- abc/71.rb
+++ abc/71.rb
@@ -6,6 +6,19 @@ else
   puts "A"
 end
 B
+a = gets.chomp
+b = []
+for num in "a".."z" do
+  if a.count(num) == 0
+    b << num
+  end 
+end
+
+if b == [] 
+  puts "None"
+else 
+  puts b[0]
+end
 C
 D
 E
