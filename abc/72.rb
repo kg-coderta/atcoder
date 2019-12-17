0b401c455438bceb53f78240ea050fcddc56e881
kg <ke123iji@gmail.com>
2019-12-17 23:07:50 +0900

    answer72

---
+++ abc/72.rb
@@ -0,0 +1,21 @@
+A
+x,t = gets.split.map(&:to_i)
+left = x - t
+if left >=0 
+  puts left
+else
+  puts 0
+end
+B
+a = gets.split("")
+b = a.length
+for num in 0..b-1 do
+  if num.even?
+    print a[num]
+  end 
+end
+
+C
+D
+E
+F
