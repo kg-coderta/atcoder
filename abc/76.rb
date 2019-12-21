b2c19023e6291cd9a6e8e979197a724b68efaad5
kg <ke123iji@gmail.com>
2019-12-21 17:01:26 +0900

    answer76

---
+++ abc/76.rb
@@ -0,0 +1,21 @@
+A
+a = gets.to_i
+b = gets.to_i
+p 2*b - a 
+
+B
+a = gets.to_i
+b = gets.to_i
+c = 1
+for num in 1..a do
+  if c < b  
+    c*= 2
+  else
+    c+= b  
+  end  
+end
+p c
+C
+D
+E
+F
