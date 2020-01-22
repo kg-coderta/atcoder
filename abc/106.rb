897a2ec20628ef955db33e3a1735c4ea6a702ef0
kg <ke123iji@gmail.com>
2020-01-22 13:49:00 +0900

    answer106

---
+++ abc/106.rb
@@ -0,0 +1,22 @@
+A
+a,b = gets.split.map(&:to_i)
+p a*b-(a+b-1)
+
+B
+require 'prime'
+a= gets.to_i
+ans = 0
+def number_divisors(n)
+  Prime.prime_division(n).map {|p, e| e + 1 }.inject(:*)
+end
+
+for num in 3..a do
+  if num.odd?
+    if number_divisors(num) == 8
+      ans += 1
+    end
+  end
+end
+p ans
+C
+D
