ed243de6eb3d0ef8d77aed84e9f2cb62c03263e3
kg <ke123iji@gmail.com>
2020-01-25 23:35:32 +0900

    answer109

---
+++ abc/109.rb
@@ -0,0 +1,32 @@
+A
+a,b = gets.split.map(&:to_i)
+ans =(a * b).odd?  ?  "Yes":  "No"
+puts ans
+
+B
+n = gets.to_i
+a =[]
+ans = "Yes"
+for num in 0..n-1 do
+words = gets.chomp
+  a << words
+end
+
+for num in 0..n-1 do
+  b = a.count(a[num])
+  if b == 2
+    ans = "No"
+  end
+end
+
+for num in 0..n-2 do
+  c = a[num].slice(-1)
+  d = a[num+1].slice(0)
+  if c != d 
+    ans = "No"
+  end
+end
+puts ans
+
+C
+D
