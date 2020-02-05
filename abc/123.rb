093ae0a51ceff714071ad6bcccfa701f2bcdfecf
kg <ke123iji@gmail.com>
2020-02-06 00:46:09 +0900

    answer123

---
+++ abc/123.rb
@@ -0,0 +1,18 @@
+A
+a = gets.to_i
+b = gets.to_i
+c = gets.to_i
+d = gets.to_i
+e = gets.to_i
+k = gets.to_i
+ar = [a,b,c,d,e]
+ar.sort
+if ar[4]-ar[0] > k
+  puts ":("
+else
+  puts "Yay!"
+end
+
+B
+C
+D
