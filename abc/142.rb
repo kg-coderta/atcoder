4be11b4e0111a0ebd535a3d3f41a9d8cc9b41ce2
kg <ke123iji@gmail.com>
2020-02-27 05:01:13 +0900

    answer142

--- abc/142.rb
+++ abc/142.rb
@@ -1,5 +1,19 @@
 A
+n = gets.to_f
+m = (n/2).ceil
+p m/n
+
 B
+n,k = gets.split.map(&:to_i)
+h = gets.split.map(&:to_i)
+ans = 0
+for num in 0..n-1 do
+  if h[num] >= k
+    ans += 1
+  end
+end
+p ans
+
 C
 D
 E
