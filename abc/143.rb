176ff262c7d0a0cd70148aa22251d326c89de1c8
kg <ke123iji@gmail.com>
2020-02-27 21:01:46 +0900

    answer143

--- abc/143.rb
+++ abc/143.rb
@@ -1,6 +1,32 @@
 A
+x = gets.split(" ")
+a= x[0].to_i
+b= x[1].to_i
+c = a - b * 2
+  if c >0
+    puts c
+  else
+    puts 0
+  end
+
 B
+n=gets.chomp.to_i
+m=gets.chomp.split.map(&:to_i)
+sum=0
+m.each do |i|
+sum += i
+end
+total=0
+m.each do |i|
+total+=i*(sum-i)
+end
+puts total/2
+
 C
+a = gets.to_i
+slime = gets.chomp.squeeze.length
+p slime
+
 D
 E
 F
