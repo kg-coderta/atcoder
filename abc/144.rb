f5884df764566e7b924651bbd6125e0b52a2477b
kg <ke123iji@gmail.com>
2020-02-28 23:27:09 +0900

    answer144

--- abc/144.rb
+++ abc/144.rb
@@ -1,5 +1,23 @@
 A
+a,b = gets.split.map(&:to_i)
+if a < 10 && b < 10
+  puts a * b
+else
+  puts -1
+end
+
 B
+n = gets.to_i
+ans = "No"
+for j in 1..9 do
+  for k in 1..9 do
+    if j * k == n
+      ans = "Yes"
+    end
+  end
+end
+puts ans
+
 C
 D
 E
