6c68d872da425c4d7a8d8dc19355442a10dbccf8
kg <ke123iji@gmail.com>
2020-03-09 00:00:23 +0900

    add hitachi test

--- abc/hitachi.rb
+++ abc/hitachi.rb
@@ -7,3 +7,23 @@ else
 end
 
 B
+a,b,m = gets.split.map(&:to_i)
+ 
+fridge = gets.split.map(&:to_i)
+micro = gets.split.map(&:to_i)
+minf = fridge.sort[0]
+minm = micro.sort[0]
+sum = minf + minm
+sum2 = 0
+sumar = []
+for j in 0..m-1 do
+  discount = gets.split.map(&:to_i)
+  sum2 = fridge[discount[0]-1] + micro[discount[1]-1] - discount[2]
+  sumar << sum2
+end
+minar =sumar.sort[0]
+if minar <= sum
+  p minar
+else
+  p sum
+end
