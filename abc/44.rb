9a4f1bb38dc3e6f71a6f1dad5ff741abfc952ee6
kg <ke123iji@gmail.com>
2020-03-12 23:44:24 +0900

    update 44

--- abc/44.rb
+++ abc/44.rb
@@ -10,7 +10,25 @@ A
         fee = n * x
       end  
     puts fee
+
 B
+w = gets.chomp.split("")
+nw = w.uniq
+le = nw.length
+check = 0
+
+for num in 0..le-1 do
+  if w.count(nw[num]).even?
+    check += 1
+  end
+end
+
+if le == check
+  puts "Yes"
+else
+  puts "No"
+end
+
 C
 D
 E
