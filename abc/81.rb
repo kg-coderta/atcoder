703d5475bc7ab9fa0536153621d0e43f1abab38d
kg <ke123iji@gmail.com>
2020-03-21 23:41:41 +0900

    add 81

--- abc/81.rb
+++ abc/81.rb
@@ -3,5 +3,17 @@ n = gets.split("")
 p n.count("1")
 
 B
+n = gets.to_i
+a = gets.split(" ").map(&:to_i)
+result = a.map do |i|
+  count = 0
+  while i % 2 == 0 do
+    i = i /2
+    count += 1
+  end
+  count
+end
+puts(result.min)
+
 C
 D
