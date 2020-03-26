69ac4af551e74c880d90c9ec0337b8c215bf7ed0
kg <ke123iji@gmail.com>
2020-03-26 23:40:49 +0900

    add 97

--- abc/97.rb
+++ abc/97.rb
@@ -1,5 +1,22 @@
 A
-B
+a,b,c,d = gets.split.map(&:to_i)
+if (a-c).abs <= d ||  ((a-b).abs <= d && (b-c).abs <= d)
+  puts "Yes"
+else
+  puts "No"
+end
+
+B(WA)
+x = gets.to_i
+a = 31
+b = []
+for num in 1..a do
+  if num**2 <= x
+  b << num**2
+  end
+end
+p b[-1]
+
 C
 D
 E
