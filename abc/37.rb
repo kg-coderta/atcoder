c1db0645c731dc32bdd662f8c184ff470d0ce9dc
kg <ke123iji@gmail.com>
2020-04-05 22:41:06 +0900

    answer37 and add 55

---
+++ abc/37.rb
@@ -0,0 +1,9 @@
+A
+a,b,c = gets.split.map(&:to_i)
+ar = [a,b].sort
+p c/ar[0]
+
+B
+
+C
+D
--- abc/55.rb
+++ abc/55.rb
@@ -1,11 +1,19 @@
 A
 n= gets.to_i
 sum = 800 * n
- rest = n / 15
+  rest = n / 15
 discount = 200 * rest
 ans = sum - discount
 puts ans
+
 B
+n = gets.to_i
+ans = 1
+for num in 1..n do
+  ans *= num
+end
+p ans % (10**9 + 7)
+
 C
 D
 E
