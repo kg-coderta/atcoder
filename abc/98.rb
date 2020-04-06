cb3ab5c301f5503c2010f1b6c5332c4690ddb12a
kg <ke123iji@gmail.com>
2020-04-06 23:45:09 +0900

    add98

--- abc/98.rb
+++ abc/98.rb
@@ -4,7 +4,25 @@ c = a + b
 d = a - b
 e = a * b
 p [c,d,e].max
+
 B
+n = gets.to_i
+s = gets.chomp
+max = 0
+
+for i in 0..n-1 do
+	a = s[0,i]
+	b = s[i..n]
+	score = 0
+	('a'..'z').map do |c|
+		score += a.include?(c) && b.include?(c) ? 1 : 0
+	end
+	if max < score
+		max = score
+	end
+end
+puts max
+
 C
 D
 E
