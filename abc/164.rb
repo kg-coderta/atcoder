3c975e91da9412d06bdce4aa7fc7ec6117c73879
kg <ke123iji@gmail.com>
2020-04-28 20:07:47 +0900

    add164

--- abc/164.rb
+++ abc/164.rb
@@ -7,7 +7,22 @@ else
   puts "safe"
 end
 
-B
+B (WA)
+a,b,c,d = gets.split.map(&:to_i)
+
+while a > 0 && c > 0
+  c -= b
+  if c <= 0
+    puts "Yes"   
+  end
+  
+  a -= d
+  if a <= 0
+    puts "No" 
+  end
+end
+
+
 C
 D
 E
