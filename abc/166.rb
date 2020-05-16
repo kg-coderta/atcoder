c942eb7f74948ef89d27489c44ccce80508e5e8c
kg <ke123iji@gmail.com>
2020-05-16 22:45:19 +0900

    answer 166

---
+++ abc/166.rb
@@ -0,0 +1,23 @@
+A
+x = gets.chomp.split("")
+if x[1]== "B"
+  puts "ARC"
+else
+  puts "ABC"
+end
+  
+B
+n,k = gets.split.map(&:to_i)
+ar = []
+for num in 0..k-1 do
+a = gets.to_i
+b = gets.split.map(&:to_i) 
+  ar << b
+end
+p n - ar.flatten.uniq.length
+
+
+C
+D
+E
+F
