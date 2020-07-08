48f4bf698e7afcef3737201397060c817edc01c9
kg <ke123iji@gmail.com>
2020-07-08 21:50:34 +0900

    answer170

--- abc/170.rb
+++ abc/170.rb
@@ -1,5 +1,22 @@
 A
+x = gets.split.map(&:to_i)
+sum = 0
+for num in 0..4 do
+sum += num
+end
+ans = 15 - sum 
+p ans
+
 B
+x,y = gets.split.map(&:to_i)
+min = x * 2
+max = x * 4
+if y >= min && y <= max && y.even?
+  puts "Yes"
+else
+  puts "No"
+end
+
 C
 D
 E
