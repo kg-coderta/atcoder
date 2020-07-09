6b3dcd5ae67c66b07f1b0235ef23a53283c5bb43
kg <ke123iji@gmail.com>
2020-07-09 23:54:04 +0900

    answer171

--- abc/171.rb
+++ abc/171.rb
@@ -1,5 +1,20 @@
 A
+x = gets.chomp
+if x.ord <=90 
+  puts "A"
+else
+  puts "a"
+end
 B
+n,k = gets.split.map(&:to_i)
+a = gets.split.map(&:to_i).sort
+
+min = 0
+for num in 0..k-1 do
+min += a[num]
+end
+p min
+
 C
 D
 E
