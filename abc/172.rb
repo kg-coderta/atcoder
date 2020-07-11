66151d90dabcb832b0d425c797bc1f3f3016c022
kg <ke123iji@gmail.com>
2020-07-11 22:43:49 +0900

    answer172

--- abc/172.rb
+++ abc/172.rb
@@ -1,6 +1,22 @@
 A
+a = gets.to_i
+p a+a**2+a**3
 
 B
+a = gets.chomp.split("")
+b = gets.chomp.split("")
+c = []
+d = []
+e = a.length
+
+correct = 0
+for num in 0..e-1 do
+  if a[num] == b[num]
+    correct +=1
+  end
+end
+p e - correct
+
 C
 D
 E
