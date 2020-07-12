191d7cbd78154a2eefdbaead6d5dee7017097013
kg <ke123iji@gmail.com>
2020-07-12 23:27:56 +0900

    answer173

--- abc/173.rb
+++ abc/173.rb
@@ -1,6 +1,30 @@
-
 A
+n = gets.to_i
+realn = n % 1000
+ans = 1000 - realn
+
+if ans == 1000
+  p 0
+else
+  p ans
+end
+
 B
+n = gets.to_i
+ar = []
+for num in 0..n-1 do
+  word = gets.chomp
+  ar << word
+end
+  ac = ar.count("AC")
+  wa = ar.count("WA")
+  tle = ar.count("TLE")
+  re = ar.count("RE")
+puts "AC x #{ac}"
+puts "WA x #{wa}"
+puts "TLE x #{tle}"
+puts "RE x #{re}"
+
 C
 D
 E
