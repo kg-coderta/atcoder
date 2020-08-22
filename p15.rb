338250fb3b71d60660f9edc36c8f49132abe3996
kg <ke123iji@gmail.com>
2020-08-22 19:55:56 +0900

    add p15

--- p15.rb
+++ p15.rb
@@ -1,4 +1,14 @@
+A 
 x = gets.to_i
 puts 1000 -(x * 180)
 
-
+B
+a,b = gets.split.map(&:to_i)
+sum = 0
+for num in 0..a-1 do
+  n,m = gets.split.map(&:to_i)
+  if n < m
+    sum += 1
+  end
+end
+p sum
